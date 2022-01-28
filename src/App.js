a3e24fef87d9217116bb5320c7b31bdfc236b8ad
taqi151214 <84099952+taqi151214@users.noreply.github.com>
2022-01-28 19:53:43 +0500

    Add files via upload
    first command 
    npm install
    then 
    npm start

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Emailer from './Components/Emailer';
+
+function App() {
+  return (
+    <div className="App">
+      <Emailer/>
+    </div>
+  );
+}
+
+export default App;
